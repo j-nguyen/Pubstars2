604867c690768de6ac8ec83bc3d4ee0f578bab36
sam2 <samhaliburton@gmail.com>
2016-11-13 23:09:39 -0400

    redid routing and added navbar links, home page and account page

--- src/Pubstars2/Controllers/HomeController.cs
+++ src/Pubstars2/Controllers/HomeController.cs
@@ -7,37 +7,17 @@ using Pubstars2.Models.PubstarsViewModels;
 
 namespace Pubstars2.Controllers
 {
-
     public class HomeController : Controller
-    {
-        private readonly SignInManager<ApplicationUser> _signInManager;
+    {      
 
         public HomeController(SignInManager<ApplicationUser> sign)
         {
-            _signInManager = sign;
+          
         }
 
         public IActionResult Index()
         {
-            //fakedata
-            List<LeaderboardEntryViewModel> entries = new List<LeaderboardEntryViewModel>();
-            for (int i = 0; i < 100; i++)
-            {
-                entries.Add(new LeaderboardEntryViewModel()
-                {
-                    name = "player " + i,
-                    rating = 1000 + (20 * i),
-                    gamesPlayed = 20,
-                    wins = 10,
-                    losses = 10,
-                    winPercentage = 50,
-                    pointsPerGame = 0.5f,
-                    points = 10,
-                    goals = 5,
-                    assists = 5
-                });
-            }
-            return View(entries.OrderByDescending(o => o.rating));
+            return View();
         }
        
     }
---
+++ src/Pubstars2/Controllers/LeaderboardController.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Pubstars2.Models.PubstarsViewModels;
+
+namespace Pubstars2.Controllers
+{
+    public class LeaderboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            //fakedata
+            List<LeaderboardEntryViewModel> entries = new List<LeaderboardEntryViewModel>();
+            for (int i = 0; i < 100; i++)
+            {
+                entries.Add(new LeaderboardEntryViewModel()
+                {
+                    name = "player " + i,
+                    rating = 1000 + (20 * i),
+                    gamesPlayed = 20,
+                    wins = 10,
+                    losses = 10,
+                    winPercentage = 50,
+                    pointsPerGame = 0.5f,
+                    points = 10,
+                    goals = 5,
+                    assists = 5
+                });
+            }
+            return View(entries.OrderByDescending(o => o.rating));            
+        }
+    }
+}
