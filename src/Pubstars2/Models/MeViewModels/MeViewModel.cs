19fa3f7cb00c4e0ed1e287543c5d7b2763c6c139
Andrew Locklair <bucsfan2@gmail.com>
2016-11-17 02:59:17 -0600

    Remove unnecessary usings

--- src/Pubstars2/Models/MeViewModels/MeViewModel.cs
+++ src/Pubstars2/Models/MeViewModels/MeViewModel.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Pubstars2.Models.MeViewModels
+﻿namespace Pubstars2.Models.MeViewModels
 {
     public class MeViewModel
     {
--- src/Pubstars2/Models/PubstarsViewModels/StatlineViewModel.cs
+++ src/Pubstars2/Models/PubstarsViewModels/StatlineViewModel.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Pubstars2.Models.PubstarsViewModels
+﻿namespace Pubstars2.Models.PubstarsViewModels
 {
     public class StatlineViewModel
     {
