5a1149ede57171210780dd0b8b7ff9db960f641f
sam2 <samhaliburton@gmail.com>
2016-11-20 22:28:18 -0400

    Added extention methods to seed database

---
+++ src/Pubstars2/Data/ApplicationDbContextSeedExtentions .cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Pubstars2.Data;
+using Pubstars2.Models.Pubstars;
+using PubstarsDtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pubstars2.Models
+{
+    public static class ApplicationDbContextSeedExtentions 
+    {
+      
+        public static async void SeedUsers(this ApplicationDbContext context)
+        {    
+            for(int i = 0; i < 25; i++)
+            {
+                string name = "player" + i;
+                if (context.Users.FirstOrDefault(x=>x.UserName == name) == null)
+                {
+                    ApplicationUser user = new ApplicationUser()
+                    {
+                        UserName = name,
+                        PubstarsPassword = "test",
+                        PlayerStats = new Pubstars.PlayerStats()
+                    };
+
+                    context.Users.Add(user);
+                }
+                               
+            }
+            
+        }
+
+        public static async void SeedGames(this ApplicationDbContext context)
+        {
+            
+        }
+    }
+}
