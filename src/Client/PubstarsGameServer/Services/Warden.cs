8d18ed797bcc40937d48f68f7c126c21129a3eb4
sam2 <samhaliburton@gmail.com>
2016-12-25 16:02:04 -0400

    Changed warden check rate to improve performance in wine

--- src/Client/PubstarsGameServer/Services/Warden.cs
+++ src/Client/PubstarsGameServer/Services/Warden.cs
@@ -65,7 +65,7 @@ namespace PubstarsGameServer.Services
                             }
                         }
                     }
-                    await Task.Yield();
+                    await Task.Delay(50);
                 }
             }      
         }  
